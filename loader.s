df28b85783f200e35603da036e6583f07e30208c
SleepingFox <seanloganking@gmail.com>
2019-10-22 21:05:46 -0400

    fix loader.s

--- loader.s
+++ loader.s
@@ -1,40 +1,40 @@
-; set magic number
-; used to tell bootloader that this is a bootable operating system
+# set magic number
+# used to tell bootloader that this is a bootable operating system
 .set MAGIC, 0x1badb002 
-; set flags for bootloader
+# set flags for bootloader
 .set FLAGS, (1<<0 | 1<<1)
-.set CHECKSUM -(MAGIC + FLAGS)
+.set CHECKSUM, -(MAGIC + FLAGS)
 
-; tell compiler to put these in .o file
+# tell compiler to put these in .o file
 .section .multiboot
     .long MAGIC
     .long FLAGS
     .long CHECKSUM
 
-; set stack pointer (boot loader didn't set it)
+# set stack pointer (boot loader didn't set it)
 .section .text
-.extern kernelMain  ;tell assembler that there is going to be something called kernelMain
+.extern kernelMain  #tell assembler that there is going to be something called kernelMain
 .global loader
 
-; program entry point
+# program entry point
 loader:
-    ; set stack pointer
-    ; $esp = stack pointer
-    mov $kernel_stack, $esp
+    # set stack pointer
+    # $esp = stack pointer
+    mov $kernel_stack, %esp
 
-    ; get pointer to info from bootloader and stores in kernel stack
+    # get pointer to info from bootloader and stores in kernel stack
     push %eax
     push %ebx
 
-    ; run the kernel
-    call kernelMain     ; never suppose to leave
+    # run the kernel
+    call kernelMain     # never suppose to leave
 
-_stop:      ; just incase program exits kernelMain
+_stop:      # just incase program exits kernelMain
     cli
     hlt
     jmp _stop
 
 
 .section .bss
-.space 2*1024*1024;      2 MiB of empty space
-kernel_stack: 
+.space 2*1024*1024 #      2 MiB of empty space
+kernel_stack:
