e52098c07e817abc008260761f400d795101e2d7
SleepingFox <seanloganking@gmail.com>
2019-10-22 20:49:25 -0400

    initialization after bootloader but before kernel

--- kernel.cpp
+++ kernel.cpp
@@ -1,5 +1,6 @@
-void kernelMain(){
+//paramaters are data retrieved from bootloader
+void kernelMain(void* multiboot_structure, unsigned int magicnumber){
     printf("Hello World!");
 
-    while(1);
+    while(1);   // so that the kernel doesn't stop
 }
--- loader.s
+++ loader.s
@@ -1,17 +1,40 @@
+; set magic number
+; used to tell bootloader that this is a bootable operating system
+.set MAGIC, 0x1badb002 
+; set flags for bootloader
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM -(MAGIC + FLAGS)
+
+; tell compiler to put these in .o file
+.section .multiboot
+    .long MAGIC
+    .long FLAGS
+    .long CHECKSUM
+
+; set stack pointer (boot loader didn't set it)
 .section .text
 .extern kernelMain  ;tell assembler that there is going to be something called kernelMain
 .global loader
 
+; program entry point
 loader:
+    ; set stack pointer
+    ; $esp = stack pointer
     mov $kernel_stack, $esp
-    call kernelMain
 
-_stop:
+    ; get pointer to info from bootloader and stores in kernel stack
+    push %eax
+    push %ebx
+
+    ; run the kernel
+    call kernelMain     ; never suppose to leave
+
+_stop:      ; just incase program exits kernelMain
     cli
     hlt
     jmp _stop
 
 
 .section .bss
-.space 2*1024*1024;     # 2 MiB
-kernel_stack:
+.space 2*1024*1024;      2 MiB of empty space
+kernel_stack: 
