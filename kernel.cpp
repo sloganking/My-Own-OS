d5020fbdc6d49b3683f4c2103c3017e631132445
SleepingFox <seanloganking@gmail.com>
2019-11-04 10:21:25 -0400

    make kernel.cpp use new types

--- kernel.cpp
+++ kernel.cpp
@@ -1,7 +1,9 @@
+#include "types.h"
+
 void printf(char* str){
     // pointer to memory location
     // GPU will auto display characters stored at 0xb8000
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
     for(int i = 0; str[i] != '\0'; ++i){
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
@@ -23,7 +25,7 @@ extern "C" void callConstructors()
 }
 
 //paramaters are data retrieved from bootloader
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber){
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber){
     printf("Hello World!");
 
     while(1);   // so that the kernel doesn't stop
