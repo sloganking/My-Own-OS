8942b3a3d6d57ca5391a974cd23f39e1eca0ccfb
SleepingFox <seanloganking@gmail.com>
2020-01-30 05:35:29 -0400

    invertCharAt()
    cleaned up redundant code

--- mouse.cpp
+++ mouse.cpp
@@ -4,6 +4,15 @@
 //so that we can use printf
 void printf(char*);
 
+void invertCharAt(uint8_t x, uint8_t y){
+
+    uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
+                            | ((VideoMemory[80*y+x] & 0x0F00) << 4)
+                            | (VideoMemory[80*y+x] & 0x00FF);
+}
+
 //constructor
 MouseDriver::MouseDriver(InterruptManager* manager)
 : InterruptHandler(0x2C, manager),      //interrupt number for mouse
@@ -13,12 +22,8 @@ commandport(0x64)
     offset = 0;
     buttons = 0;
 
-    uint16_t* VideoMemory = (uint16_t*)0xb8000;
-
     //at the start flip the character in center of screen (represents mouse)
-    VideoMemory[80*12+40] = ((VideoMemory[80*12+40] & 0xF000) >> 4)
-                            | ((VideoMemory[80*12+40] & 0x0F00) << 4)
-                            | (VideoMemory[80*12+40] & 0x00FF);
+    invertCharAt(40,12);
 
     commandport.Write(0xA8); // activate interrupts
     commandport.Write(0x20); // get current state of PIC
@@ -51,12 +56,8 @@ uint32_t MouseDriver::HandleInterrupt(uint32_t esp){
 
     if(offset == 0){
 
-        static uint16_t* VideoMemory = (uint16_t*)0xb8000;
-
         //invert colors of old cursor position back to normal
-        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
-                            | ((VideoMemory[80*y+x] & 0x0F00) << 4)
-                            | (VideoMemory[80*y+x] & 0x00FF);
+        invertCharAt(x,y);
 
         //buff 1 == movement on x axis
         x += buffer[1];
@@ -69,17 +70,13 @@ uint32_t MouseDriver::HandleInterrupt(uint32_t esp){
         if(y >= 25) y = 24;
 
         //invert colors at new cursor position
-        VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
-                            | ((VideoMemory[80*y+x] & 0x0F00) << 4)
-                            | (VideoMemory[80*y+x] & 0x00FF);
+        invertCharAt(x,y);
 
 
         for(uint8_t i = 0; i < 3; i++){
             //if buttons are different from last buttons
             if((buffer[0] & (0x01 << i)) != (buttons & (0x01<<i))){
-                VideoMemory[80*y+x] = ((VideoMemory[80*y+x] & 0xF000) >> 4)
-                            | ((VideoMemory[80*y+x] & 0x0F00) << 4)
-                            | (VideoMemory[80*y+x] & 0x00FF);
+                invertCharAt(x,y);
             }
         }
         buttons = buffer[0];
