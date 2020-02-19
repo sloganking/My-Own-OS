ba542d8a5f5e5c4c712adf951171ccb47be53585
SleepingFox <seanloganking@gmail.com>
2020-02-19 00:00:11 -0400

    Create Widget Objects

--- myos/include/gui/widget.h
+++ myos/include/gui/widget.h
@@ -44,6 +44,36 @@ namespace myos{
                 virtual void OnKeyDown(common::int32_t x, common:: int32_t y);
                 virtual void OnKeyUp(common::int32_t x, common:: int32_t y);
         };
+
+        //contains array of widgets and the order decides which gets drawn first
+        class CompositeWidget : public Widget{
+            private: 
+                Widget* children[100];
+                int numChildren;
+                Widget* focussedChild;
+
+            public:
+
+                //constructor
+                CompositeWidget(
+                    Widget* parent, 
+                    common::int32_t x, common::int32_t y, common::int32_t w, common::int32_t h, 
+                    common::uint8_t r, common::uint8_t g, common::uint8_t b
+                );
+
+                //destructor
+                ~CompositeWidget();
+
+                virtual void GetFocus(Widget* widget);
+
+                virtual void Draw(GraphicsContext* gc);
+                virtual void OnMouseDown(common::int32_t x, common:: int32_t y);
+                virtual void OnMouseUp(common::int32_t x, common:: int32_t y);
+                virtual void OnMouseMove(common::int32_t oldx, common:: int32_t oldy, common::int32_t newx, common:: int32_t newy);
+
+                virtual void OnKeyDown(common::int32_t x, common:: int32_t y);
+                virtual void OnKeyUp(common::int32_t x, common:: int32_t y);
+        };
     }
 }
 
--- myos/src/gui/widget.cpp
+++ myos/src/gui/widget.cpp
@@ -80,10 +80,124 @@ void Widget::OnMouseMove(int32_t oldx,  int32_t oldy, int32_t newx,  int32_t new
 
 }
 
-void Widget::OnKeyDown(int32_t x,  int32_t y){
+void Widget::OnKeyDown(char* str){
 
 }
 
-void Widget::OnKeyUp(int32_t x,  int32_t y){
+void Widget::OnKeyUp(char* str){
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//constructor
+CompositeWidget::CompositeWidget(
+    Widget* parent, 
+    int32_t x, int32_t y, int32_t w, int32_t h, 
+    uint8_t r, uint8_t g, uint8_t b
+)
+{
+    focussedChild = 0;
+    numChildren = 0;
+}
+
+//destructor
+CompositeWidget::~CompositeWidget(){
+
+}
+
+void CompositeWidget::GetFocus(Widget* widget){
+    this->focussedChild = widger;
+    if(parent != 0){
+        parent->GetFocus(this);
+    }
+}
+
+void CompositeWidget::Draw(GraphicsContext* gc){
+    //draw own background
+    Widget::Draw(gc);
+
+    //draw all children
+    for(int i = numChildren; i >= 0; --i){
+        children[i]->Draw(gc);
+    }
+}
+
+//iterate through children and pass event to the child that contains the coorinate
+//pass the click to the one that has been clicked
+void CompositeWidget::OnMouseDown(int32_t x,  int32_t y){
+    for(int i = 0; i < numChildren; ++i){
+        if(children[i]->ContainsCoordinate(x - this->x, y - this->y)){
+            children[i]->OnMouseDown(x - this->x, y - this->y);
+
+            //only the top window is clicked on
+            break;
+        }
+    }
+}
+
+//iterate through children and pass event to the child that contains the coorinate
+//pass the click to the one that has been clicked
+void CompositeWidget::OnMouseUp(int32_t x,  int32_t y){
+    for(int i = 0; i < numChildren; ++i){
+        if(children[i]->ContainsCoordinate(x - this->x, y - this->y)){
+            children[i]->OnMouseUp(x - this->x, y - this->y);
+
+            //only the top window is clicked on
+            break;
+        }
+    }
+}
+
+void CompositeWidget::OnMouseMove(int32_t oldx,  int32_t oldy, int32_t newx,  int32_t newy){
+    
+    int firstChild = -1;
+
+    //for object that contains old coordinate
+    for(int i = 0; i < numChildren; ++i){
+        if(children[i]->ContainsCoordinate(oldx - this->x, oldy - this->y)){
+
+            //subtracting thisx/y turns the coordinates into relative coordinates
+            children[i]->OnMouseMove(oldx - this->x, oldy - this->y, newx - this->x, newy - this->y);
+
+            firstChild = i;
+
+            //only the top window is clicked on
+            break;
+        }
+    }
+
+    //for object that contains new coordinate
+    for(int i = 0; i < numChildren; ++i){
+        if(children[i]->ContainsCoordinate(newx - this->x, newy - this->y)){
+
+            if(firstChild != i){
+                //subtracting thisx/y turns the coordinates into relative coordinates
+                children[i]->OnMouseMove(oldx - this->x, oldy - this->y, newx - this->x, newy - this->y);
+            }
+
+            //only the top window is clicked on
+            break;
+        }
+    }
+}
+
+void CompositeWidget::OnKeyDown(char* str){
+    if(focussedChild != 0){
+        focussedChild->OnKeyDown(str);
+    }
+}
+void CompositeWidget::OnKeyUp(char* str){
+    if(focussedChild != 0){
+        focussedChild->OnKeyUp(str);
+    }
 }
