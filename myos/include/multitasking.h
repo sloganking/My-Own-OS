c0d2a2b837e4baf47b429ea82f6e563cf2d54324
SleepingFox <seanloganking@gmail.com>
2020-03-06 05:10:57 -0400

    Update multitasking.h

--- myos/include/multitasking.h
+++ myos/include/multitasking.h
@@ -3,11 +3,36 @@
 
 namespace myos{
     class Task{
+        friend class TaskManager;
+        private:
 
+            //4kB of stack memory
+            common::uint8_t stack[4096];    
+
+            //cpu pushed and user pushed data
+            CPUState* cpustate
+
+        public:
+            //constructor
+            Task(GlobalDescriptorTable *gdt, void entrypoint());
+
+            //destructor
+            ~Task();
     };
 
     class TaskManager{
+        private:
+            Task* tasks[256];
+            int numTasks;
+            int currentTask;
+        public:
+            //constructor
+            TaskManager();
 
+            //destructor
+            ~TaskManager();
+            bool AddTask(Task* task);
+            CPUState* Schedule(CPUstate* cpustate);
     };
 }
 
