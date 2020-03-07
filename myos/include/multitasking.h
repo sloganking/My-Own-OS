73ecc0377e3f3846676424b4991937f0c9862e5c
SleepingFox <seanloganking@gmail.com>
2020-03-07 04:15:09 -0400

    Update multitasking.h

--- myos/include/multitasking.h
+++ myos/include/multitasking.h
@@ -1,7 +1,41 @@
 #ifndef __MYOS__MULTITASKING_h
 #define __MYOS__MULTITASKING_h
 
+#include <common/types.h>
+#include <gdt.h>
+
 namespace myos{
+
+    struct CPUState{
+        
+        //we have to manually push these
+        
+            //general purpose registers
+            common::uint32_t eax;
+            common::uint32_t ebx;
+            common::uint32_t ecx;
+            common::uint32_t edx;
+
+            common::uint32_t esi;   //stack index
+            common::uint32_t edi;   //data index
+            common::uint32_t ebp;   //base pointer
+
+            common::uint32_t gs;
+            common::uint32_t fs;
+            common::uint32_t es;
+            common::uint32_t ds;
+
+            common::uint32_t error;
+
+        //automatically pushed by the processor
+            common::uint32_t eip;   //instruction pointer
+            common::uint32_t cs;    //code segment
+            common::uint32_t eflags;    //flags
+            common::uint32_t esp;   //stack pointer
+            common::uint32_t ss;    //stack segment
+
+    } __attribute__((packed));
+
     class Task{
         friend class TaskManager;
         private:
